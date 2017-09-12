d41128cad10c1a5956f9c08b9cbafaf90cbf5ad5
Alex Mekelburg <alex@mekware.com>
2017-09-12 09:05:21 -0400

    setup tracking for fake endpoint

--- Gemfile
+++ Gemfile
@@ -1,2 +1,14 @@
 source 'https://rubygems.org'
+gem 'rake'
 gem 'sinatra'
+gem "activerecord"
+gem "sinatra-activerecord"
+
+group :development do
+ gem 'sqlite3'
+ gem "tux"
+end
+
+group :production do
+ gem 'pg'
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
 post '/SureOVRWebAPI/api/ovr' do
-"<RESPONSE><APPLICATIONID>-1</APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><ERROR></ERROR></RESPONSE>"
+  
+  request.body.rewind
+  request_payload = JSON.parse request.body.read
+  
+  
+  @request_xml = request_payload["ApplicationData"]
+  Post.create!(xml_request: @request_xml)
+  is_error = @request_xml =~ /<FirstName>ERROR<\/FirstName>/
+  if is_error
+      "<RESPONSE><APPLICATIONID></APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR>There was an error</ERROR></RESPONSE>"
+  else
+    "<RESPONSE><APPLICATIONID>010101</APPLICATIONID><APPLICATIONDATE>#{DateTime.now}</APPLICATIONDATE><SIGNATURE></SIGNATURE><ERROR></ERROR></RESPONSE>"
+  end
+end
+
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  erb :"posts/index"
 end
